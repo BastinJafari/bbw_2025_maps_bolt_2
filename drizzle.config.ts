9a8a872ca576f44b0780c8fc5b14b318d41f9ee7
Bastin <BastinJafari@gmail.com>
2025-06-20 05:02:16 +0200

    feat: move schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 import 'dotenv/config';
 
 export default defineConfig({
-  schema: './lib/schema.ts',
+  schema: './lib/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
---
+++ lib/db/schema.ts
@@ -0,0 +1,9 @@
+// This is the main schema file.
+// It exports all the other schema files in this directory. 
+
+export * from './schema/users';
+export * from './schema/countries';
+export * from './schema/cities';
+export * from './schema/products';
+export * from './schema/clients';
+export * from './schema/etc';
